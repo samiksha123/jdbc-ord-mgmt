637250b7eb5f81227164308883ca6ba2cbc05147
samiksha <samiksha@thoughtworks.com>
2014-01-25 14:23:19 +0530

    src files removed

--- test/samiksha/jdbc_ord_mgmt/OrdMgmtTest.java
+++ test/samiksha/jdbc_ord_mgmt/OrdMgmtTest.java
@@ -1,10 +1,12 @@
-package samiksha.jdbc_ord_mgmt.lib;
+package samiksha.jdbc_ord_mgmt;
+
+import java.sql.*;
+
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.sql.*;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,22 +20,22 @@ public class OrdMgmtTest {
     @Before
     public void setUp() throws Exception {
         try{
-        Class.forName(url);
-        con = DriverManager.getConnection(db_path, "samiksha", "samiksha@ande");
-        stmt = con.createStatement();
-        String query = "CREATE SCHEMA order_management";
-        stmt.execute(query);
+            Class.forName(url);
+            con = DriverManager.getConnection(db_path, "samiksha", "samiksha@ande");
+            stmt = con.createStatement();
+            String query = "CREATE SCHEMA order_management";
+            stmt.execute(query);
 
-        String query2 = "CREATE TABLE order_management.customer(cust_id varchar(20) primary key,cust_name varchar(20),address1 varchar(20))";
-        stmt.execute(query2);
+            String query2 = "CREATE TABLE order_management.customer(cust_id varchar(20) primary key,cust_name varchar(20),address1 varchar(20))";
+            stmt.execute(query2);
 
-        String query3 = "CREATE TABLE order_management.product(p_id int primary key,pname varchar(20)" +
-            ",unit_price int,p_category varchar(15))";
-        stmt.execute(query3);
+            String query3 = "CREATE TABLE order_management.product(p_id int primary key,pname varchar(20)" +
+                    ",unit_price int,p_category varchar(15))";
+            stmt.execute(query3);
 
-        String query4 = "CREATE TABLE order_management.orders(o_id int primary key,pid int" +
-                ",o_date date,quantity int,total_price int,o_status char(1))";
-        stmt.execute(query4);
+            String query4 = "CREATE TABLE order_management.orders(o_id int primary key,pid int" +
+                    ",o_date date,quantity int,total_price int,o_status char(1))";
+            stmt.execute(query4);
 
             System.out.println("created successfully");
         }catch (SQLException e){
@@ -72,3 +74,4 @@ public class OrdMgmtTest {
         }
     }
 }
+
